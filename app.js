647764839fe76818d72d01263601f2ac7ae07273
Mahirr <167292271+mahiirr@users.noreply.github.com>
2025-01-02 22:55:24 +0530

    Add Logic of win,lose and draw game

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
 
+const drawgame  = ()=>{
+  console.log("It's Draww.");
+}
+
+const showwiner = (userwin) =>{
+    if(userwin){
+        console.log("you Win!");
+    }else{
+        console.log("You Lose.")
+    }
+}
 const gencompChoice = () =>{
     const options =["Rock","Paper","Scissor"];
     const randInx = Math.floor(Math.random()* 3);
@@ -14,6 +25,22 @@ const playgame = (userchoice) => {
     //Generate comp Choice  
     const compChoice = gencompChoice();
     console.log("compChoice =", compChoice);
+
+    if(userchoice === compChoice){
+        drawgame();
+    }else{
+        let userwin = true;
+        if(userchoice === "rock"){
+            //paper scissor 
+            userchoice = compChoice === "paper" ? false : true; 
+        }else if(userchoice === "paper"){
+            //rock scissor
+            userchoice = compChoice === "Scissor" ? false : true;
+        }else{
+           userchoice = compChoice ==="rock" ? false : true;
+        }
+        showwiner(userwin);
+    }
 }
 
 choices.forEach((choice) => {

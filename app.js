c169031fc4d26274b63f3a40870d50db175220c3
Mahirr <167292271+mahiirr@users.noreply.github.com>
2025-01-02 23:30:24 +0530

    error fixed of lose condition

--- app.js
+++ app.js
@@ -3,46 +3,47 @@ let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
 
-const drawgame  = ()=>{
-  console.log("It's Draww.");
-}
-
-const showwiner = (userwin) =>{
-    if(userwin){
-        console.log("you Win!");
-    }else{
-        console.log("You Lose.")
-    }
-}
 const gencompChoice = () =>{
     const options =["Rock","Paper","Scissor"];
     const randInx = Math.floor(Math.random()* 3);
     return options[randInx]; 
 }
 
+const drawgame  = () => {
+  console.log("It's Draww.");
+}
+ 
+const showwiner = (userwin) =>{
+    if(userwin){
+        console.log("you Win!");
+    }else{
+      console.log("You Lose.");
+    }
+}
+
 const playgame = (userchoice) => {
-    console.log("userChoice = ",userchoice);
-    //Generate comp Choice  
+    console.log("user Choice = ",userchoice);
+
+    //Generate comp Choice   
     const compChoice = gencompChoice();
-    console.log("compChoice =", compChoice);
+    console.log("comp Choice =", compChoice);
 
     if(userchoice === compChoice){
         drawgame();
-    }else{
+    } else {
         let userwin = true;
-        if(userchoice === "rock"){
+        if(userchoice === "Rock"){
             //paper scissor 
-            userchoice = compChoice === "paper" ? false : true; 
-        }else if(userchoice === "paper"){
+            userwin = compChoice === "Paper" ? false : true; 
+        }else if(userchoice === "Paper"){
             //rock scissor
-            userchoice = compChoice === "Scissor" ? false : true;
-        }else{
-           userchoice = compChoice ==="rock" ? false : true;
+            userwin = compChoice === "Scissor" ? false : true;
+        }else {
+           userwin = compChoice === "Rock" ? false : true;
         }
         showwiner(userwin);
     }
 }
-
 choices.forEach((choice) => {
     choice.addEventListener("click",() =>{
         const userchoice = choice.getAttribute("id");
